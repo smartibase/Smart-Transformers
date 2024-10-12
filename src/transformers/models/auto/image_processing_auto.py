huggingface/transformers

Initial commit with template code generated by transformers-cli

src/transformers/models/auto/image_processing_auto.py
@@ -89,6 +89,7 @@ IMAGE_PROCESSOR_MAPPING_NAMES = OrderedDict(
         ("pvt", "PvtImageProcessor"),
         ("regnet", "ConvNextImageProcessor"),
         ("resnet", "ConvNextImageProcessor"),
+        ("superglue", "ConvNextImageProcessor"),
         ("sam", "SamImageProcessor"),
         ("segformer", "SegformerImageProcessor"),
         ("swiftformer", "ViTImageProcessor"),

smartibase/Smart-Transformers

src/transformers/models/auto/image_processing_auto.py
@@ -128,6 +128,7 @@ else:
             ("segformer", ("SegformerImageProcessor",)),
             ("seggpt", ("SegGptImageProcessor",)),
             ("siglip", ("SiglipImageProcessor",)),
+            ("superglue", "SuperGlueImageProcessor"),
             ("swiftformer", ("ViTImageProcessor", "ViTImageProcessorFast")),
             ("swin", ("ViTImageProcessor", "ViTImageProcessorFast")),
             ("swin2sr", ("Swin2SRImageProcessor",)),